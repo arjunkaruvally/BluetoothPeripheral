88cb6375fe6d14c96124db06c5a4bde3fb7bf26a
arjun23496 <arjun.k018@gmail.com>
2016-02-19 22:27:55 +0530

    Added a crude version of the computer side of the application functioning as device driver for the bluetooth phone

---
+++ Computer Side/windowsBluetooth.py
@@ -0,0 +1,38 @@
+import keyboardEvents
+from bluetooth import *
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+uuid = "714d9764-afd6-44ca-8913-de270903f1ab"
+advertise_service( server_sock, "SampleServer",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ], 
+                   )
+
+print("Waiting for connection on RFCOMM channel %d" % port)
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from ", client_info)
+
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if len(data) == 0: break
+        result=data.decode("utf-8")
+
+        if result=="space" :        
+        	keyboardEvents.PressKey(32);
+        	keyboardEvents.ReleaseKey(32);
+        
+
+        print("received [%s]" % result)
+except IOError:
+    pass
+
+print("disconnected")
+client_sock.close()
+server_sock.close()
+print("all done")
